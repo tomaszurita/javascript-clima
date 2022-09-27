f5fe6609fd4ca64e2f042b150c37f37ae8455bb5
Tomas Zurita Sorgetti <tomaszuritasorgetti@gmail.com>
2022-09-26 23:11:50 -0300

    Local Storage

--- scripts/main.js
+++ scripts/main.js
@@ -62,4 +62,13 @@ cityForm.addEventListener('submit', e => {
   updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
+    
+  //Local storage.
+  localStorage.setItem('city', city); 
 });
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
